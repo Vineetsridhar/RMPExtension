96391988b902cb79a8f1d2e42f71ebeb87eb0821
Vineet Sridhar <vineet.sridhar@glyde.app>
2020-08-31 00:25:06 -0400

    Finished front end chrome app

--- frontend/background.js
+++ frontend/background.js
@@ -23,38 +23,27 @@ function makeGraphQLQuery(name) {
     })
 }
 
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        active_tab = tab.tabId;
-        if (current_tab_info.url == "https://uisapppr3.njit.edu/scbldr/") {
-            chrome.tabs.insertCSS(null, {file:'./styles.css'})
-            chrome.tabs.executeScript(null, { file: './foreground.js' })
-        }
-    })
-})
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     if (request.name.length > 1) {
-        try {
-            let x = await makeGraphQLQuery(request.name);
-            let json = await x.json()
-
-            json = json["data"]["professor"];
-            if(!json.length) throw new Error();
-
-            json = json[0];
-
-            let name = json["name"];
-            let rating = json["rating"];
-            let rate = rating["rating"];
-            let diff = rating["difficulty"];
-            let retake = rating["retake"];
-            let tid = json["tid"];
-
-            chrome.tabs.sendMessage(active_tab, {name, rate, diff, retake, tid})
-        } catch (err) {
-            alert("Could not find this professor")
-        }
-        alert(JSON.stringify(json))
+        makeGraphQLQuery(request.name)
+            .then(x => x.json())
+            .then(json => {
+                json = json["data"]["professor"];
+                if (!json.length) throw new Error();
+                json = json[0];
+                let name = json["name"];
+                let rating = json["rating"];
+                let rate = rating["rating"];
+                let diff = rating["difficulty"];
+                let retake = rating["retake"];
+                let tid = json["tid"];
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                    chrome.tabs.sendMessage(tabs[0].id, { name, rate, diff, retake, tid }, function(response) {});  
+                });
+            }).catch(err => {
+                alert("Could not find this professor")
+            })
     }
+    return true;
 })
---
+++ frontend/content.js
@@ -0,0 +1,57 @@
+//Create modal
+const modal = document.createElement('dialog');
+modal.setAttribute("style", "border-radius:10px");
+modal.innerHTML =
+    `<iframe id="headlineFetcher" style="height:250px" frameBorder="0"></iframe>
+            <div class="modalContainer">
+                <button>x</button>
+                <h1 class="professorName"></h1>
+                <h2 class="ratingMain"></h2>
+                <p class="difficulty"></p>
+                <p class="retake"></p>
+                <a class="linkToRMP">View Professor's page</a>
+            </div>`;
+document.body.appendChild(modal);
+const dialog = document.querySelector("dialog");
+
+//Check if new class was added
+let numProf = 0;
+setInterval(() => {
+    let elements = document.getElementsByClassName("sec-instructor");
+    if(elements.length < numProf) numProf = elements.length;
+    else if(elements.length > numProf) injectHTML(elements)
+}, 500)
+
+//create button 
+function createButton(name, dialog) {
+    let button = document.createElement('button');
+    button.className = "btn"
+    button.innerText = "View Professor";
+    button.addEventListener('click', () => {
+        chrome.runtime.sendMessage({ name })
+    })
+    return button
+}
+
+//
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    dialog.querySelector("h1").innerText = request.name
+    dialog.getElementsByClassName("ratingMain")[0].innerText = request.rate + "/5.0";
+    dialog.getElementsByClassName("difficulty")[0].innerText = "Difficulty: " + request.diff + "/5.0";
+    dialog.getElementsByClassName("retake")[0].innerText = request.retake + "% would take again";
+    dialog.getElementsByClassName("linkToRMP")[0].innerHTML = `<a target="_blank" href=https://www.ratemyprofessors.com/ShowRatings.jsp?tid=${request.tid}>View Professor's page</a>`
+    dialog.querySelector("button").addEventListener("click", () => {
+        dialog.close();
+    });
+    dialog.showModal()
+})
+
+
+
+function injectHTML(elements) {
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].innerText && !elements[i].innerHTML.includes("button")) {
+            elements[i].append(createButton(elements[i].innerText, dialog));
+        }
+    }
+}
